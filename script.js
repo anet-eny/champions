e6ae6ca28fe75cb24be71bd372b1c38269133e0d
anet-eny <anet.eny@gmail.com>
2024-05-06 21:07:56 +0200

    Set onValue function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://champions-788f0-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -17,12 +17,21 @@ buttonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     push(endorsementsInDB, inputValue)
     console.log(inputValue)
-    clearInputField()
-    endorsementsListEl.innerHTML += `<li>${inputValue}</li>`
-
-
+    clearInputFieldEl()
 })
 
-function clearInputField() {
+onValue(endorsementsInDB, function(snapshot) {
+    let itemsArray = Object.values(snapshot.val())
+    
+    for(let i = 0; i < itemsArray.length; i++) {
+        appendItemToEndorsementsListEl(itemsArray[i])
+    }
+})
+
+function clearInputFieldEl() {
     inputFieldEl.value = ""
+}
+
+function appendItemToEndorsementsListEl(item) {
+    endorsementsListEl.innerHTML += `<li>${item}</li>`
 }
