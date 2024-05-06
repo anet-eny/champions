0207c08dd849014002d33134a67362671d748e19
anet-eny <anet.eny@gmail.com>
2024-05-06 20:45:04 +0200

    Add push function and list element

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://champions-788f0-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -11,8 +11,18 @@ const endorsementsInDB = ref(database, "endorsements")
 
 const inputFieldEl = document.getElementById("input-field")
 const buttonEl = document.getElementById("btn")
+const endorsementsListEl = document.getElementById("endorsements-list")
 
 buttonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
+    push(endorsementsInDB, inputValue)
     console.log(inputValue)
+    clearInputField()
+    endorsementsListEl.innerHTML += `<li>${inputValue}</li>`
+
+
 })
+
+function clearInputField() {
+    inputFieldEl.value = ""
+}
