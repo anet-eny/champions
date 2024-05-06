35f8c3ed8993bb22647946da535d9be936a435e4
anet-eny <anet.eny@gmail.com>
2024-05-06 13:51:47 +0200

    Add and set Firebase

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://champions-788f0-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const endorsementsInDB = ref(database, "endorsements")
+
 const inputFieldEl = document.getElementById("input-field")
 const buttonEl = document.getElementById("btn")
 
